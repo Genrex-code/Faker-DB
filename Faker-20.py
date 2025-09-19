faaac2a74eb8ea375bd484c9238de4e9712875af
genrex-code <jairo2030antonio007@gmail.com>
2025-09-19 17:34:10 -0600

    no c si que carajos hice pero por alguna razon lo hice funcionar con mysql

--- Faker-20.py
+++ Faker-20.py
@@ -1,10 +1,25 @@
 from faker import Faker
-import sqlite3
 ### ingreso de datos
 nombre_bd = input("Escribe el nombre de la base de datos: ")
+##ponle INT a los cosos que sean puro numero
+num_tablas = int(input("Cuantas tablas deseas?"))
 nombre_tabla = input("Escribe el nombre de la tabla: ")
 num_columnas = int(input("¿Cuántas columnas tendrá la tabla? "))
 num_registros = int(input("¿Cuántos registros deseas generar? "))
+coneccion = input("desea el archivo para carga manual o carga en servidor? (manual/servidor):")
+if coneccion.lower() == "servidor":
+    #le puse una S para que se vea mas bonito
+    host_S = input("Escribe el host (ej: localhost): ")
+    user_S = input("Escribe el usuario de la base de datos: ")
+    password_S = input("Escribe la contraseña del usuario: ")
+else:
+    print("se generara el archivo para carga manual")
+#aca agrego la capacidad de crear varias tablas aunque no se si funcione bien
+#porque no lo he probado y me quede sin cereal :D
+tablas = []
+for i in range(num_tablas):
+               nombre_tabla = input(f"Escribe el nombre de la tabla {i+1}: ")
+               tablas.append (f"{nombre_tabla}")
 #limitante para evitar que esta madre truene
 #aca se consideran sanos alrededor de 4 columnas solamente
 LIMITE_REGISTROS = 100000
@@ -58,61 +73,61 @@ for _ in range(num_registros):
             elif "telefono" in nombre_col.lower() or "tel" in nombre_col.lower() or "celular" in nombre_col.lower():
                 valores.append(f"'{fake.phone_number()}'")
             elif "ciudad" in nombre_col.lower():
-                valores.append(f"{fake.city()}")
+                valores.append(f"'{fake.city()}'")
             elif "pais" in nombre_col.lower():
-                valores.append(f"{fake.country()}")
+                valores.append(f"'{fake.country()}'")
             elif "direccion" in nombre_col.lower() or "departamento" in nombre_col.lower():
-                valores.append(f"{fake.address()}")
+                valores.append(f"'{fake.address()}'")
             elif "cumpleaños" in nombre_col.lower():
-                valores.append(f"{fake.date_of_birth()}")
+                valores.append(f"'{fake.date_of_birth()}'")
             elif "codigo postal" in nombre_col.lower():
-                valores.append(f"{fake.postal_code()}")
+                valores.append(f"'{fake.postal_code()}'")
             elif "curp" in nombre_col.lower():
-                valores.append(f"{fake.curp()}")
+                valores.append(f"'{fake.curp()}'")
             elif "nif" in nombre_col.lower():
-                valores.append(f"{fake.nif()}")
+                valores.append(f"'{fake.nif()}'")
             elif "ssn" in nombre_col.lower():
-                valores.append(f"{fake.ssn()}")
+                valores.append(f"'{fake.ssn()}'")
             elif "username" in nombre_col.lower():
-                valores.append(f"{fake.user_name()}")
+                valores.append(f"'{fake.user_name()}'")
             elif "compañia" in nombre_col.lower():
-                valores.append(f"{fake.company()}")
+                valores.append(f"'{fake.company()}'")
             elif "trabajo" in nombre_col.lower():
-                valores.append(f"{fake.job()}")
+                valores.append(f"'{fake.job()}'")
             elif "banco" in nombre_col.lower() or "caja popular" in nombre_col.lower():
-                valores.append(f"{fake.bank()}")
+                valores.append(f"'{fake.bank()}'")
             elif "fecha" in nombre_col.lower():
-                valores.append(f"{fake.date()}")
+                valores.append(f"'{fake.date()}'")
             elif "color" in nombre_col.lower() or "colores" in nombre_col.lower():
-                valores.append(f"{fake.color()}")
+                valores.append(f"'{fake.color()}'")
             elif "lenguaje" in nombre_col.lower() or "idioma" in nombre_col.lower():
-                valores.append (f"{fake.language_name()}")
+                valores.append (f"'{fake.language_name()}'")
             elif "tajeta credito" in nombre_col.lower() or "tarjeta debito" in nombre_col.lower():
-                valores.append (f"{fake.credit_card_number()}")
+                valores.append (f"'{fake.credit_card_number()}'")
             elif "estado civil" in nombre_col.lower():
-                valores.append(f"{fake.civil_status()}")
+                valores.append(f"'{fake.civil_status()}'")
 #como tal no existia en de poner salario asi que se usa el ramdon numero int para eso por lo que si sale muy 
 # ramdon culpa del programa no del pobre programador que esta perdido en quien sabe dios donde haciendo esto 
 # en ves de cuidarse que no lo asalten y le quiten todo aunque no traiga nada el pobre pndjo
             elif "salario"in nombre_col.lower():
-                valores.append(f"{fake.random_int}")
+                valores.append(f"'{fake.random_int}'")
             elif "contratacion" in nombre_col.lower() or "fecha de contratacion" in nombre_col.lower():
-                valores.append(f"{fake.date_this_century}")
+                valores.append(f"'{fake.date_this_century}'")
             elif "producto" in nombre_col.lower():
-                valores.append(f"{fake.items}")
+                valores.append(f"'{fake.items}'")
             #aca va el corte
             else:
                 valores.append(f"'{fake.word()}'")
                 #aca coloque un limitante para el generador de numero si no truena esta chingadera
         elif "INT" in tipo_col:
-            valores.append(f"{fake.random_int(min=1, max=1000)}")
+            valores.append(f"'{fake.random_int(min=1, max=1000)}'")
         else:
             valor = (f"'{fake.word()}'") #valor por defecto si no se reconoce el tipo o el usuario esta muy baboso y no sabe escribir bien porque le muy tarado tiene down o algo asi
     #aca se inserta al 
     sql_script += f"""
 INSERT INTO {nombre_tabla}({", ".join([col.split()[0] for col in columnas])}) VALUES ({", ".join(valores)});
 """
-
+import sqlite3
 # Guardar script en archivo .sql
 with open("script_generado.sql", "w", encoding="utf-8") as archivo:
     archivo.write(sql_script)
@@ -121,3 +136,9 @@ with open("script_generado.sql", "w", encoding="utf-8") as archivo:
 print("\n Script SQL generado con éxito en 'script_generado.sql'")
 print("------ Vista previa ------")
 print(sql_script)
+import mysql.connector
+conexion = mysql.connector.connect(
+    host=host_S,  #si esto truena es porque perdieron las chivas
+    user=user_S,    
+    password= password_S
+)
---
+++ script_generado.sql
@@ -0,0 +1,30 @@
+
+CREATE DATABASE IF NOT EXISTS demo1;
+USE demo1;
+
+CREATE TABLE IF NOT EXISTS demo3 (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ine varchar(12),
+     ssn Varchar(100),
+     cumpleaños varchar(100)
+);
+
+INSERT INTO demo3(ine, ssn, cumpleaños) VALUES ('nuestro', '92485282609', '1968-10-12');
+
+INSERT INTO demo3(ine, ssn, cumpleaños) VALUES ('aquel', '68061728181', '2006-08-09');
+
+INSERT INTO demo3(ine, ssn, cumpleaños) VALUES ('pública', '57536592454', '1964-04-26');
+
+INSERT INTO demo3(ine, ssn, cumpleaños) VALUES ('movimiento', '45683545276', '1988-09-21');
+
+INSERT INTO demo3(ine, ssn, cumpleaños) VALUES ('garcía', '69528692408', '1977-06-16');
+
+INSERT INTO demo3(ine, ssn, cumpleaños) VALUES ('efecto', '51632845098', '1955-12-07');
+
+INSERT INTO demo3(ine, ssn, cumpleaños) VALUES ('miedo', '28157798050', '1930-12-08');
+
+INSERT INTO demo3(ine, ssn, cumpleaños) VALUES ('público', '01794127140', '1953-08-30');
+
+INSERT INTO demo3(ine, ssn, cumpleaños) VALUES ('central', '98185006446', '1987-09-10');
+
+INSERT INTO demo3(ine, ssn, cumpleaños) VALUES ('marcha', '31749751892', '1960-04-27');

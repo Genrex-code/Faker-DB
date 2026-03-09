79cfd08a845e83763ccf59eb5da37adf563b1e47
genrex-code <jairo2030antonio007@gmail.com>
2026-03-09 00:27:18 -0600

    creacion a medio hacer de personal yh del main chiyoso

---
+++ .vscode/src/generators/personal.py
@@ -0,0 +1,59 @@
+from faker import Faker
+from F_dic.faker_chingadere import capacidades , fake
+import random
+import pandas as pd 
+import numpy as np
+import string 
+from main import NumFila,tipocolumnas,nombre_col,COLUMNAS
+fake = Faker ('es_MX')
+#recordar agregar el cambiar de idioma
+#nota les meti abreviaciones porque si
+# para que se vea mas chingon poes :3
+#amn es = amount o cantidad en inglish para hacernos los 
+#que si sabesmos aunque estemos vagnado con esto
+
+#busca en el diccionario y ejecuta la funcion
+def natural (tipo,NumFila):
+    #para buscar
+    if tipo in capacidades:
+        return [capacidades[tipo]() for i in range(NumFila)]
+    else:
+        return fake.word(),["ERROR:NO SE ENCONTRO UN TIPO EXACTO SE APLICARA UN GENERATIVO GENERICO"]
+    
+def BIGINT (NumFila,tipocolumna,nombre_col):
+    name = nombre_col.lower()
+    type = tipocolumna.lower()
+    amn = NumFila
+    if "bigint" in type:
+        return random.randint(1,100000)
+
+def BIGDEC (NumFila,tipocolumna,nombre_col):
+    name = nombre_col.lower()
+    type = tipocolumna.lower()
+    amn = NumFila
+    if "bigdec" in type:
+        return random.uniform(1.0,100000.0)
+
+def BIGSTR (NumFila,tipocolumna,nombre_col):
+    name = nombre_col.lower()
+    type = tipocolumnas.lower()
+    amn = NumFila
+    if "bigstr" in type:
+        return random.choice(string.ascii_letters)
+
+#dinamico dinamistico
+#neta como me gusta la musica de linkin park 
+#la nueva vocalista es wena y nadie me llevara la contraria >:l
+def ARMADO (NumFila,tipocolumna,nombre_col):
+    #lista basia para que no truene 
+    dataset = {}
+    for col in COLUMNAS:
+        #aca se hace la buscqueda de lo que se escribio el usuario en la terminal (luego interfaz)
+        #en nuestro diccionario
+        if col in capacidades:
+            dataset[col] = [capacidades[col]() for _ in range (NumFila)]
+            #debi haber usado un elif pero nooo queria hacerme el que si sabia
+            #duerman al programador y regresenlo al kinder
+        else:
+            dataset [col] = ["Dato no soporto 💋"] * NumFila
+    return dataset
---
+++ .vscode/src/main.py
@@ -0,0 +1,59 @@
+#imports rancios
+
+
+print("Generador de base de datos sinteticos")
+print("Crea datos para pruebas en ambientes controlados :3")
+#si ya se que queda algo inutil pero aca agregare mas limitantes en el futuro cuando
+# se tengan que manejar muchos mas datos de forma provicional se queda asi (como la casa del boiler)
+# si leen esto WEBOS
+#duerman al programador de esto esta sufriendo mucho
+
+def LIMITANTE(maximofila):
+    maximofila = 1000000
+    return maximofila
+
+def pedir_entero (mensaje):
+    while True:
+        try:
+            valor = int(input(mensaje))
+            if valor <= 0:
+                print("ingresa un numero mayor a 0")
+            else:
+                return valor
+        except:
+            ValueError("el dato es incorrecto")
+
+while True:
+    NameDataset= str(input("Ingresa el nombre de tu DATASET: "))
+    if NameDataset == "":
+        print ("El nombre no puede estar vacio")
+        if NameDataset == " ":
+            print("El Dataset no puede tener espacios")
+    else:
+        break
+    #PREGUNTAR COLUMNAS
+while True:
+    try:
+        NumCol = int(input("Cuantas Columnas Desea???: "))
+        break # es un limitante si salio bien 
+    
+    except  ValueError: 
+        print("Error: SOLO SE ADMITEN NUMEROS D:")
+#preguntar el nombre de cada columna y de aca sacar el tipo de dato natural a generar
+#asi mapeo 2 datos de uno 
+COLUMNAS = []
+for _ in range (NumCol):
+    nombre_col = input(f"Como se llamara su columna {_+1} ???")
+    tipocolumnas = input(f"Que tipo de dato nesesita para la columna {_+1}")
+    #unimos el desmadre
+    COLUMNAS.append(f"{nombre_col} {tipocolumnas}")
+# preguntar filas 
+while True:
+     try:
+        NumFila = int(input("Cuantas Filas Nesesitas???"))
+        break
+     except ValueError:
+         print("ERROR: SOLO SE ADMITEN NUMEROS D:")
+if NumFila > LIMITANTE(maximofila=1000000):
+    print(f"el numero de registros excede el limte {LIMITANTE} se ajustara al limite")
+    NumFila = LIMITANTE
